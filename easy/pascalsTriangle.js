e95ba3b37cafcbfd08a854d340cfe6ab465f124d
ghoshshreya <shrghosh08@gmail.com>
2023-03-04 11:33:17 +0530

    1 file modified, 1 added

---
+++ easy/pascalsTriangle.js
@@ -0,0 +1,26 @@
+/*----------GENERATE PASCALS' TRIANGLE----------*/
+var generate = function (numRows) {
+  const arr = [];
+  if (numRows > 0) {
+    arr.push([1]);
+    if (numRows > 1) {
+      arr.push([1, 1]);
+      for (let x = 2; x < numRows; x++) {
+        const mini = new Array(x);
+        mini[0] = mini[x] = 1;
+        let i = 1;
+        let j = x - 1;
+        while (i <= j) {
+          const sum = arr[x - 1][i - 1] + arr[x - 1][i];
+          mini[i] = mini[j] = sum;
+          i++;
+          j--;
+        }
+        arr.push(mini);
+      }
+    }
+  }
+  return arr;
+};
+
+console.log(pascalsTriangle(5));
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import './easy/seondSmallestAndLargest.js';
 import './easy/checkIfArrayIsSorted.js';
 import './easy/leftShiftByOne.js';
 import './easy/leftShiftByAnIndex.js';
+import './easy/pascalsTriangle.js';
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
