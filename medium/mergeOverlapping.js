56c2b02e2ee1dde36098367d0b5217247a3230ee
ghoshshreya <shrghosh08@gmail.com>
2023-03-05 15:18:52 +0530

    1 file modified

--- medium/mergeOverlapping.js
+++ medium/mergeOverlapping.js
@@ -1,15 +1,16 @@
 var merge = function (intervals) {
-  console.log('Hello');
   let overlapping = [];
   intervals.sort(function (a, b) {
     return a[0] - b[0];
   });
 
-  for (let x = 0; x < intervals.length - 1; x++) {
+  console.log(intervals);
+
+  for (let x = 0; x < intervals.length; x++) {
     let ov = [intervals[x][0], intervals[x][1]];
-    let count = x + 1;
+    console.log(ov);
+    let count = x;
     for (let y = x + 1; y < intervals.length; y++) {
-      console.log(intervals[x], intervals[y]);
       if (
         intervals[x][0] < intervals[y][1] &&
         intervals[x][1] > intervals[y][0]
@@ -17,20 +18,22 @@ var merge = function (intervals) {
         ov[0] = intervals[x][0];
         ov[1] = intervals[y][1];
         count++;
-        console.log('Test', ov);
       } else {
         break;
       }
     }
-    console.log(count);
-    x = count;
+
     overlapping.push(ov);
+
+    x = count;
+    console.log(x);
   }
 
   return overlapping;
 };
 
 console.log(
+  'Merged',
   merge([
     [1, 3],
     [8, 16],
