ca25e1f0f790e74fc2c85f97bd27d6aeb3cccb56
ghoshshreya <shrghosh08@gmail.com>
2023-03-10 00:33:56 +0530

    1 file modified

--- easy/leftShiftByAnIndex.js
+++ easy/leftShiftByAnIndex.js
@@ -3,9 +3,15 @@
  * Output [5,6,1,2,3,4]
  */
 function leftShiftByKElements(nums, k) {
-  let temp1 = nums.slice(0, nums.length - k); // 0, 4 -> 1,2,3,4
-  let temp2 = nums.slice(nums.length - k, nums.length); // 4,6 -> 5, 6
-  nums = [...temp2, ...temp1];
-  console.log(nums);
+  function leftShift(nums) {
+    let firstElem = nums.shift();
+    nums.push(firstElem);
+    if (k > 0) {
+      leftShift(nums);
+      k--;
+    }
+  }
+
+  return leftShift(nums);
 }
 console.log('Hello', leftShiftByKElements([1, 2, 3, 4, 5, 6], 2));
